a9c539a6cf5eeed17447b937921539516e4cc755
Theodore Rieger <ted.rieger@pfizer.com>
2017-12-15 15:09:10 -0500

    Fixed NHANES correlation plot.
    Last piece in squashing bug introduced the last time I tried to plot the data.

--- NHANES/correlate_nhanes_chol.m
+++ NHANES/correlate_nhanes_chol.m
@@ -1,4 +1,4 @@
-function [mvmodel, r] = correlate_nhanes_chol(do_plots)
+function [mdl, r] = correlate_nhanes_chol(do_plots)
 %clear;clc;close all;rng('shuffle');
 if nargin == 0
     do_plots = 0;
@@ -23,72 +23,82 @@ r = [lnHDL lnLDL lnTC]; % Gather the variables of interest
 r = r(~any(isnan(r'))',:); % Clear any rows with NaN, not absolutely necessary, but avoids a warning message
 c = corrcoef(r);
 % Fit a multivariate Gaussian model to the log-transformed data:
-mvmodel = fitgmdist(r,1); % model parameters should match mean(r) and cov(r).
+mdl = fitgmdist(r,1); % model parameters should match mean(r) and cov(r).
+labels = {'log(HDL)';'log(LDL)';'log(TC)'};
 
 fs1 = 28;
-
 if do_plots
-    error('Plots are incorrect --- FIXME'); % FIXME TODO BUG
-%     figure('Name','Correlations','Units','Inches','Position',[1 1 20 10]);
-%     subplot(3,3,1);
-%     histogram(lnHDL);
-%     ylabel('log(HDL)');
-%     title('log(HDL)');
-%     set(gca,'Box','on','XTick',[],'XTickLabel',[],'YTick',[],'YTickLabel',[],'FontSize',fs1);
-%     subplot(3,3,2);
-%     title('log(LDL)');
-%     set(gca,'Box','on','XTick',[],'XTickLabel',[],'YTick',[],'YTickLabel',[],'FontSize',fs1);
-%     %plot(lnHDL,lnLDL,'o');
-%     subplot(3,3,3);
-%     title('log(TC)');
-%     set(gca,'Box','on','XTick',[],'XTickLabel',[],'YTick',[],'YTickLabel',[],'FontSize',fs1);
-%     %plot(lnTC,lnLDL,'o');
-%     subplot(3,3,4);
-%     plot(lnLDL,lnHDL,'o');
-%     ylabel('log(HDL)');
-%     set(gca,'Box','on','XTick',[],'XTickLabel',[],'YTick',[],'YTickLabel',[],'FontSize',fs1);
-%     subplot(3,3,5);
-%     histogram(lnHDL);
-%     set(gca,'Box','on','XTick',[],'XTickLabel',[],'YTick',[],'YTickLabel',[],'FontSize',fs1);
-%     subplot(3,3,6);
-%     %plot(lnTC,lnHDL,'o');
-%     subplot(3,3,7);
-%     plot(lnLDL,lnTC,'o');
-%     set(gca,'Box','on','XTick',[],'XTickLabel',[],'YTick',[],'YTickLabel',[],'FontSize',fs1);
-%     ylabel('log(TC)');
-%     xlabel('log(LDL)');
-%     subplot(3,3,8);
-%     plot(lnHDL,lnTC,'o');
-%     xlabel('log(HDL)');
-%     set(gca,'Box','on','XTick',[],'XTickLabel',[],'YTick',[],'YTickLabel',[],'FontSize',fs1);
-%     subplot(3,3,9);
-%     histogram(lnTC);
-%     xlabel('log(TC)');
-%     set(gca,'Box','on','XTick',[],'XTickLabel',[],'YTick',[],'YTickLabel',[],'FontSize',fs1);
-%     subplot(3,3,2);
-%     text(0.5,0.5,...
-%         strjoin({'\rho = ' num2str(round(c(1,2),1))}),...
-%         'HorizontalAlignment','center','FontSize',28);
-%     set(gca,'XTick',[],'XTickLabel',[],'YTick',[],'YTickLabel',[],'Box','on');
-%     subplot(3,3,3);
-%     text(0.5,0.5,...
-%         strjoin({'\rho = ' num2str(round(c(1,3),1))}),...
-%         'HorizontalAlignment','center','FontSize',28);
-%     set(gca,'XTick',[],'XTickLabel',[],'YTick',[],'YTickLabel',[],'Box','on');
-%     subplot(3,3,6);
-%     text(0.5,0.5,...
-%         strjoin({'\rho = ' num2str(round(c(2,3),1))}),...
-%         'HorizontalAlignment','center','FontSize',28);
-%     set(gca,'XTick',[],'XTickLabel',[],'YTick',[],'YTickLabel',[],'Box','on');
-%     
-%     fprintf('Joint Gaussian Fit\nlnHDL\tlnLDL\tlnTC\nmu:\n');
-%     disp(mvmodel.mu);
-%     fprintf('covariance matrix:\n');
-%     disp(mvmodel.Sigma);
-%     fprintf('correlation matrix:\n');
-%     disp(corrcoef(r));
+    figure('Name','Correlations','Units','Inches','Position',[1 1 20 10]);
+    
+    subplot(3,3,1);
+    histogram(r(:,1));
+    ylabel(labels{1});
+    title(labels{1});
+    set(gca,'Box','on','XTick',[],'XTickLabel',[],'YTick',[],'YTickLabel',[],'FontSize',fs1);
+    
+    subplot(3,3,2);
+    title(labels{2});
+    set(gca,'Box','on','XTick',[],'XTickLabel',[],'YTick',[],'YTickLabel',[],'FontSize',fs1);
+    
+    subplot(3,3,3);
+    title(labels{3});
+    set(gca,'Box','on','XTick',[],'XTickLabel',[],'YTick',[],'YTickLabel',[],'FontSize',fs1);
+    
+    % Row 2:
+    subplot(3,3,4);
+    plot(r(:,2),r(:,1),'o');
+    ylabel(labels{2});
+    set(gca,'Box','on','XTick',[],'XTickLabel',[],'YTick',[],'YTickLabel',[],'FontSize',fs1);
+    
+    subplot(3,3,5);
+    histogram(r(:,2));
+    set(gca,'Box','on','XTick',[],'XTickLabel',[],'YTick',[],'YTickLabel',[],'FontSize',fs1);
+    
+    subplot(3,3,6);
+    
+    % Row 3:
+    subplot(3,3,7);
+    plot(r(:,3),r(:,1),'o');
+    set(gca,'Box','on','XTick',[],'XTickLabel',[],'YTick',[],'YTickLabel',[],'FontSize',fs1);
+    ylabel(labels{3});
+    xlabel(labels{1});
+    
+    subplot(3,3,8);
+    plot(r(:,2),r(:,3),'o');
+    xlabel(labels{2});
+    set(gca,'Box','on','XTick',[],'XTickLabel',[],'YTick',[],'YTickLabel',[],'FontSize',fs1);
+    
+    subplot(3,3,9);
+    histogram(r(:,3));
+    xlabel(labels{3});
+    
+    % Upper-diagonal correlations:
+    set(gca,'Box','on','XTick',[],'XTickLabel',[],'YTick',[],'YTickLabel',[],'FontSize',fs1);
+    subplot(3,3,2);
+    text(0.5,0.5,...
+        strjoin({'\rho = ' num2str(round(c(1,2),1))}),...
+        'HorizontalAlignment','center','FontSize',28);
+    set(gca,'XTick',[],'XTickLabel',[],'YTick',[],'YTickLabel',[],'Box','on');
+    
+    subplot(3,3,3);
+    text(0.5,0.5,...
+        strjoin({'\rho = ' num2str(round(c(1,3),1))}),...
+        'HorizontalAlignment','center','FontSize',28);
+    set(gca,'XTick',[],'XTickLabel',[],'YTick',[],'YTickLabel',[],'Box','on');
+    
+    subplot(3,3,6);
+    text(0.5,0.5,...
+        strjoin({'\rho = ' num2str(round(c(2,3),1))}),...
+        'HorizontalAlignment','center','FontSize',28);
+    set(gca,'XTick',[],'XTickLabel',[],'YTick',[],'YTickLabel',[],'Box','on');
+    
+    % Diagnostics on fitting:
+    fprintf('Joint Gaussian Fit\nlnHDL\tlnLDL\tlnTC\nmu:\n');
+    disp(mdl.mu);
+    fprintf('covariance matrix:\n');
+    disp(mdl.Sigma);
+    fprintf('correlation matrix:\n');
+    disp(corrcoef(r));
 end
-% Clear the raw file data:
-%clear thdl ttc ttrig
 
 end
